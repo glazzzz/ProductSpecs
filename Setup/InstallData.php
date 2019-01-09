c6b29eee408824eefa1a04e1541b66e9195648c9
Alex <glazzzz@nm.ru>
2019-01-09 17:04:46 +0300

    initial commit

---
+++ Setup/InstallData.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace liaonau\ProductSpecs\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Eav setup factory
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'capacity',
+            [
+                'group' => 'General',
+                'type' => 'text',
+                'label' => 'Capacity',
+                'input' => 'text',
+                'source' => '',
+                'frontend' => '',
+                'backend' => '',
+                'required' => false,
+                'sort_order' => 50,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => true,
+                'is_html_allowed_on_front' => true,
+                'visible_on_front' => true
+            ]
+        );
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'thickness',
+            [
+                'group' => 'General',
+                'type' => 'text',
+                'label' => 'Leaf Thickness',
+                'input' => 'text',
+                'source' => '',
+                'frontend' => '',
+                'backend' => '',
+                'required' => false,
+                'sort_order' => 50,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => true,
+                'is_html_allowed_on_front' => true,
+                'visible_on_front' => true
+            ]
+        );
+    }
+}
