27a098b27cc499ddea5927e066406861bacd58f3
Alex <glazzzz@nm.ru>
2019-01-09 23:25:39 +0300

    added spec

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -37,11 +37,11 @@ class InstallData implements InstallDataInterface
         $eavSetup = $this->eavSetupFactory->create();
         $eavSetup->addAttribute(
             \Magento\Catalog\Model\Product::ENTITY,
-            'capacity',
+            'specs',
             [
                 'group' => 'Content',
                 'type' => 'text',
-                'label' => 'Capacity',
+                'label' => 'Specs',
                 'input' => 'text',
                 'source' => '',
                 'frontend' => '',
@@ -49,35 +49,14 @@ class InstallData implements InstallDataInterface
                 'required' => false,
                 'sort_order' => 50,
                 'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                'is_used_in_grid' => false,
-                'is_visible_in_grid' => false,
-                'is_filterable_in_grid' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
                 'visible' => true,
                 'is_html_allowed_on_front' => true,
                 'visible_on_front' => true
             ]
         );
-        $eavSetup->addAttribute(
-            \Magento\Catalog\Model\Product::ENTITY,
-            'thickness',
-            [
-                'group' => 'General',
-                'type' => 'text',
-                'label' => 'Leaf Thickness',
-                'input' => 'text',
-                'source' => '',
-                'frontend' => '',
-                'backend' => '',
-                'required' => false,
-                'sort_order' => 50,
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                'is_used_in_grid' => false,
-                'is_visible_in_grid' => false,
-                'is_filterable_in_grid' => false,
-                'visible' => true,
-                'is_html_allowed_on_front' => true,
-                'visible_on_front' => true
-            ]
-        );
+
     }
 }
